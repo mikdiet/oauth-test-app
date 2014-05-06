b12112777a6bf5bbd685f6ffcac7310ef5961598
Mikdiet <mikdiet@gmail.com>
2014-05-06 11:17:15 +0300

    github oauth

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,11 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.generators do |g|
+    g.stylesheets false
+    g.javascripts false
+    g.helper false
+  end
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
